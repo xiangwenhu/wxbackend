29a3030c2242da2f66badde25dd9c501a7a23b94
胡祥文 <huxiangwen@che001.com>
2017-06-14 15:48:53 +0800

    https和添加测试/user

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var fs = require('fs');
+var https = require('https');
+var privateKey = fs.readFileSync('./private.pem', 'utf8');
+var certificate = fs.readFileSync('./file.crt', 'utf8');
+var credentials = { key: privateKey, cert: certificate };
+
+
+var app = express();
+
+var httpsServer = https.createServer(credentials, app);
+
 var proxy = require('http-proxy-middleware');
 
 //静态资源
 app.use(express.static(__dirname + '/public'))
+
 
 //c.y.qq.com QQ音乐
 app.use('/cyqq', proxy({
@@ -18,6 +29,26 @@ app.use('/cyqq', proxy({
     }
 }));
 
+app.get('/user', function (req, res) {
+  res.send('Got a PUT request at /user');
+});
 
 
-app.listen(80)
+app.all('*', function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+
+    if (req.method == 'OPTIONS') {
+        res.send(200); //让options请求快速返回
+    }
+    else {
+        next();
+    }
+});
+
+
+
+httpsServer.listen(443, function () {
+    console.log('HTTPS Server is running on: https://localhost:%s', 443);
+});
